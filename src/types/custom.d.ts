bed1ea7a39f2937828ba76d04b132909fa09afda
Oleksandr Myronchuk <smironcuk35@gmail.com>
2022-10-21 20:21:25 +0300

    created users-list

---
+++ src/types/custom.d.ts
@@ -0,0 +1,21 @@
+declare module "*.svg" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.svg" {
+  import React = require("react");
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
